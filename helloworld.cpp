1af6f0677e2dbdcb311469f514bec21d6db1ddf9
Percifal <esbarker@usc.edu>
2017-01-11 17:44:39 -0800

    Update helloworld.cpp
    Adding extra output line.

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
-int main(int argc, char* argv[]){
+int main(int argc, char* argv[])
+{
 	std::cout << "Hello world!" << std::endl;
-
+	std::cout << "CS 104 Rules!" << std::endl;
+	
 	return 0;
 }
